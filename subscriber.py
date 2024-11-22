d25b0a2affa493f415626c768b7f223ea4e31c7d
drewpri411 <andrewprince4111@gmail.com>
2024-11-23 00:52:10 +0800

    subscriber class has been completed and tested.

--- subscriber.py
+++ subscriber.py
@@ -1,34 +1,38 @@
 import asyncio
-import sys
 import websockets
 import os
-
 from azure.messaging.webpubsubservice import WebPubSubServiceClient
 
-async def connect(url):
-    async with websockets.connect(url) as ws:
-        print('connected')
-        while True:
-            print('Received message: ' + await ws.recv())
+class Subscriber:
+    def __init__(self, hub_name):
+        self.connection_string = os.getenv('AZURE_STRING')
+        if not self.connection_string:
+            raise ValueError("AZURE_STRING environment variable not set.")
+        self.hub_name = hub_name
+        self.service_client = None
+        self.token = None
+
+    def connect(self):
+        self.service_client = WebPubSubServiceClient.from_connection_string(self.connection_string, hub=self.hub_name)
+        self.token = self.service_client.get_client_access_token()
+        print(f"Connected to hub '{self.hub_name}'. Token generated.")
+
+    async def subscribe(self):
+        if not self.token:
+            raise ValueError("Subscriber is not connected. Call `connect()` first.")
+        async with websockets.connect(self.token['url']) as ws:
+            print("Connected to WebSocket")
+            while True:
+                message = await ws.recv()
+                print(f"Received message: {message}")
+
 
 if __name__ == '__main__':
-
-    if len(sys.argv) != 1:
-        print(len(sys.argv))
-        print('Usage: python subscribe.py')
-        exit(1)
-
-    connection_string = os.getenv('AZURE_STRING')
-    if not connection_string:
-        print(f'Error: AZURE_STRING environment variable not set.')
-        exit(1)
-
     hub_name = "logger"
-
-    service = WebPubSubServiceClient.from_connection_string(connection_string, hub=hub_name)
-    token = service.get_client_access_token()
+    subscriber = Subscriber(hub_name)
+    subscriber.connect()
 
     try:
-        asyncio.get_event_loop().run_until_complete(connect(token['url']))
+        asyncio.get_event_loop().run_until_complete(subscriber.subscribe())
     except KeyboardInterrupt:
-        pass
+        print("\nDisconnected from WebSocket.")
