490382bba572f5203531a01b787d1cb5daa8b65e
drewpri411 <andrewprince4111@gmail.com>
2024-12-04 02:31:32 +0800

    LogDate and LogGen classes are generated to help publisher send mock Log data

---
+++ log_generator.py
@@ -0,0 +1,62 @@
+import random
+import datetime
+
+class LogData:
+    def __init__(self, timestamp, log_type, message):
+        self.timestamp = timestamp
+        self.log_type = log_type
+        self.message = message
+
+    def __str__(self):
+        return f"{self.timestamp} {self.message}"
+
+
+class LogGenerator:
+    def __init__(self, num_logs=100, timespan_minutes=10):
+        self.num_logs = num_logs
+        self.timespan_minutes = timespan_minutes
+        self.start_time = datetime.datetime.utcnow()
+
+    def generate_logs(self):
+        log_types = ["INFO", "WARNING", "ERROR"]
+        logs = []
+
+        for _ in range(self.num_logs):
+            timestamp = self.start_time + datetime.timedelta(
+                seconds=random.randint(0, self.timespan_minutes * 60)
+            )
+            log_type = random.choice(log_types)
+            message = self._generate_message(log_type)
+            logs.append(LogData(timestamp.isoformat() + "Z", log_type, message))
+
+        return logs
+
+    def _generate_message(self, log_type):
+        if log_type == "INFO":
+            return self._generate_info_message()
+        elif log_type == "WARNING":
+            return self._generate_warning_message()
+        elif log_type == "ERROR":
+            return self._generate_error_message()
+
+    def _generate_info_message(self):
+        messages = [
+            "\"POST /APICALL HTTP/1.1\" 200 9426 \"-\" \"ORIGIN\"",
+            "INFO in FILE: RANDOMTEXTINFO",
+            "\"GET /APICALL HTTP/1.1\" 200 9426 \"-\" \"ORIGIN\"",
+        ]
+        return random.choice(messages)
+
+    def _generate_warning_message(self):
+        messages = [
+            "INFO in login: Login attempted by:USER",
+            "WARNING in FILENAME: Clinic ID blank for session: USER",
+            "INFO in login: USER: PASSWORD login successful for: 110.175.74.138, 66.248.202.12:63928",
+        ]
+        return random.choice(messages)
+
+    def _generate_error_message(self):
+        messages = [
+            "ERROR in patient_route: Traceback (most recent call last):\nFile \"FILENAME\", line 418, in FUNCTION\nERRONEOUS CODE\nFile \"FILENAME\", line 114, in FUNCTION\nERRONEOUS CODE\nAttributeError: 'REASON FOR ERROR'",
+        ]
+        return random.choice(messages)
