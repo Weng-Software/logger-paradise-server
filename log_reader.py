4debf4930cb79012312a3ec841db420672f13c9f
drewpri411 <andrewprince4111@gmail.com>
2024-12-04 02:37:40 +0800

    Log Reader class is added to help simulate processing

---
+++ log_reader.py
@@ -0,0 +1,21 @@
+import datetime, time
+from log_generator import LogGenerator, LogData
+from publisher import Publisher
+
+class LogReader:
+    def __init__(self, logs, publisher, speedup_factor=60):
+        self.logs = sorted(logs, key=lambda x: x.timestamp)
+        self.publisher = publisher
+        self.speedup_factor = speedup_factor
+
+    def read_logs(self):
+        if not self.logs:
+            print("No logs to read.")
+            return
+
+        start_time = datetime.datetime.fromisoformat(self.logs[0].timestamp[:-1])
+        for log in self.logs:
+            current_time = datetime.datetime.fromisoformat(log.timestamp[:-1])
+            elapsed_real_time = (current_time - start_time).total_seconds() / self.speedup_factor
+            time.sleep(elapsed_real_time)
+            self.publisher.publish_log(log)
